7a3dd7d0c6bb1954101721eadb845bdfc59c8265
Carolina Barbosa da Conceição <carolina.barbosa.conceicao@gmail.com>
2023-06-07 11:15:49 -0300

    Convertendo estilização para Styled Components

---
+++ src/styles.ts
@@ -0,0 +1,46 @@
+import styled, { createGlobalStyle } from 'styled-components'
+
+const EstiloGlobal = createGlobalStyle`
+  :root {
+    --cor-principal: #a7727d;
+    --cor-secundaria: #f9f5e7;
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: Lato, sans-serif;
+    list-style: none;
+  }
+
+  body {
+    padding-bottom: 120px;
+  }
+
+  .container {
+    max-width: 1024px;
+    width: 100%;
+    margin: 0 auto;
+  }
+
+  @media (max-width: 1024px) {
+    .container {
+      max-width: 80%;
+    }
+  }
+`
+
+export default EstiloGlobal
+
+export const Vagas = styled.ul`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  column-gap: 16px;
+  row-gap: 16px;
+  margin-top: 32px;
+
+  @media (max-width: 768px) {
+    grid-template-columns: 1fr;
+  }
+`
